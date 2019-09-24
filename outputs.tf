e8d3f954573d03766f6aeb279fa5eedfd6ef6d46
Nikita Dugar <nikita@clouddrove.com>
2019-09-24 13:14:17 +0530

    change output syntax

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,12 @@
 # Module      : SQS
 # Description : Terraform module to create SQS resource on AWS for managing queue.
 output "id" {
-  value = concat(
-    aws_sqs_queue.default.*.id
-  )[0]
+  value = join("", sqs_queue.default.*.id)
   description = "The URL for the created Amazon SQS queue."
 }
 
 output "arn" {
-  value = concat(
-    aws_sqs_queue.default.*.arn
-  )[0]
+  value = join("", sqs_queue.default.*.arn)
   description = "The ARN of the SQS queue."
 }
 
